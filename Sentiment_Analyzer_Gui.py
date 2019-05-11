8a782b66ba48ab097373f15570f7845e29e54b50
DurkoMatko <nitramdurcek@gmail.com>
2019-05-11 09:41:42 +0200

    fix: wordclouds images resolution/size

--- Sentiment_Analyzer_Gui.py
+++ Sentiment_Analyzer_Gui.py
@@ -298,6 +298,10 @@ def createCharts():
 	maskFolder = config.get('FolderTree', 'maskFolder')
 	reportFileName = config.get('FolderTree', 'reportsFolder') + "/" + config.get('FolderTree', 'reportFile')
 
+
+	wordcloudGenerator = Wordcloud_Generator(config.get('Wordcloud', 'commonWords'), reportFileName, config.get('FolderTree', 'tweetsFolder'))
+	wordcloudGenerator.createWordcloud(chartsFolder, maskFolder, maxCloudWords.get(), borderDate.get())
+
 	plotter = Charts_Plotter(chartsFolder=chartsFolder, reportFileName=reportFileName)
 	plotter.sentimentLinechart()
 	#plotter.LinePlot()
@@ -306,9 +310,6 @@ def createCharts():
 	plotter.HeatMap()
 	plotter.HeatMapWeekly()
 	#plotter.Autocorrelation()
-
-	wordcloudGenerator = Wordcloud_Generator(config.get('Wordcloud', 'commonWords'), reportFileName, config.get('FolderTree', 'tweetsFolder'))
-	wordcloudGenerator.createWordcloud(chartsFolder, maskFolder, maxCloudWords.get(), borderDate.get())
 
 
 def executeAllSteps():
